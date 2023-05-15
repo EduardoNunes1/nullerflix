d5b833a9046f237dc6fc525984eecc6619adfd79
Eduardo Nunes <eduardonunes8349@gmail.com>
2023-05-15 09:39:11 -0300

    Atualizando a classe Genre e criando a classe Movie

--- NullerFlix/Models/Genre.cs
+++ NullerFlix/Models/Genre.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NullerFlix.Models;
 
 public class Genre
 {
-    
+    [Key]
+    public byte Id { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string Name { get; set; }
+
 }
