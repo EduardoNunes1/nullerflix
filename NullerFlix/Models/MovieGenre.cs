0bb71befbf1d2f6be589f17934141ca0257571e3
Eduardo Nunes <eduardonunes8349@gmail.com>
2023-07-31 21:35:21 -0300

    Finalização do Projeto

--- NullerFlix/Models/MovieGenre.cs
+++ NullerFlix/Models/MovieGenre.cs
@@ -1,12 +1,18 @@
-namespace NullerFlix.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace GalloFlix.Models;
+[Table("MovieGenre")]
 public class MovieGenre
 {
-     public int MovieId { get; set; }
+    [Key, Column(Order = 1)]
+    public int MovieId { get; set; }
+    [ForeignKey("MovieId")]
+    public Movie Movie { get; set; }
 
-     public Movie Movie { get; set; }
-
-     public byte GenreId { get; set; }
-     
-     public Genre Genre { get; set; }
+    [Key, Column(Order = 2)]
+    public byte GenreId { get; set; }
+    [ForeignKey("GenreId")]
+    public Genre Genre { get; set; }
 }
+
